c571e290cf4ffd1c88ee16cfde4002fc7062272c
Barrsum <mugdharam89@gmail.com>
2025-09-25 05:09:54 +0530

    Modified admin components and pages to better fit new ui

--- src/pages/admin/AppSettings.jsx
+++ src/pages/admin/AppSettings.jsx
@@ -1,11 +1,17 @@
+// src/pages/admin/AppSettings.jsx
+
 import SettingsManager from '@/components/admin/SettingsManager';
 
 const AppSettings = () => {
     return (
-        <div>
-            <h1 className="text-3xl font-bold mb-6">Application Settings</h1>
+        <div className="space-y-8">
+             <div>
+                <h1 className="text-3xl font-bold">Application Settings</h1>
+                <p className="text-muted-foreground">Manage global settings and features.</p>
+            </div>
             <SettingsManager />
         </div>
     );
 };
+
 export default AppSettings;
